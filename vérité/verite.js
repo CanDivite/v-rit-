28645db784aafabc1621d6e06546b80b77993d8b
VERITE <magloireck12@gmail.com>
2025-09-22 01:23:47 +0000

    Add files via upload

---
+++ vérité/verite.js
@@ -0,0 +1,12 @@
+
+
+
+
+<script>
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+
+  menuToggle.addEventListener('click', () = {
+    menu.classList.toggle('active')
+  });
+</script>
